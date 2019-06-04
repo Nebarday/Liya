75073a5c61b80a59f7cfc49e5dcc769dcd58ccad
Nebarday <41545042+Nebarday@users.noreply.github.com>
2019-06-04 22:15:30 +0300

    Add files via upload

---
+++ Liya.py
@@ -0,0 +1,176 @@
+#Лия подражатель 1.0 BETA
+import os
+import pyttsx3
+import speech_recognition as sr
+
+print ("Лия подражатель 1.0 BETA")
+
+engine = pyttsx3.init()
+engine.say("Привет, я Лия и я буду повторять все, что вы напишете!!! У меня не много циклов поэтому иногда надо будет перезапускать меня!!! удачи, повеселитесь!!!")
+engine.runAndWait()
+
+def speak(what):
+    print( what )
+    speak_engine.say( what )
+    speak_engine.runAndWait()
+    speak_engine.stop()
+
+engine.say("что мне повторить" )
+engine.runAndWait()
+
+r = input('Что мне повторить: ')
+
+if r == "я дура":
+	engine.say("Пошел нахуй мелкий соцыопад недоразвитый с таким тупым текстом как")
+engine.runAndWait()
+
+
+engine.say(r)
+engine.runAndWait()
+
+engine.say("продолжаем" )
+engine.runAndWait()
+
+r = input('Что мне повторить: ')
+
+engine.say(r)
+engine.runAndWait()
+
+engine.say("" )
+engine.runAndWait()
+
+r = input('Что мне повторить: ')
+
+engine.say(r)
+engine.runAndWait()
+
+engine.say("ты знаешь что делать" )
+engine.runAndWait()
+
+r = input('Что мне повторить: ')
+
+engine.say(r)
+engine.runAndWait()
+
+engine.say("походу это на долго" )
+engine.runAndWait()
+
+r = input('Что мне повторить: ')
+
+engine.say(r)
+engine.runAndWait()
+
+engine.say("" )
+engine.runAndWait()
+
+r = input('Что мне повторить: ')
+
+engine.say(r)
+engine.runAndWait()
+
+engine.say("" )
+engine.runAndWait()
+
+r = input('Что мне повторить: ')
+
+engine.say(r)
+engine.runAndWait()
+
+engine.say("Не надоело?" )
+engine.runAndWait()
+
+r = input('Что мне повторить: ')
+
+engine.say(r)
+engine.runAndWait()
+
+engine.say("" )
+engine.runAndWait()
+
+r = input('Что мне повторить: ')
+
+engine.say(r)
+engine.runAndWait()
+
+engine.say("" )
+engine.runAndWait()
+
+r = input('Что мне повторить: ')
+
+engine.say(r)
+engine.runAndWait()
+
+engine.say("" )
+engine.runAndWait()
+
+r = input('Что мне повторить: ')
+
+engine.say(r)
+engine.runAndWait()
+
+engine.say("" )
+engine.runAndWait()
+
+r = input('Что мне повторить: ')
+
+engine.say(r)
+engine.runAndWait()
+
+engine.say("" )
+engine.runAndWait()
+
+r = input('Что мне повторить: ')
+
+engine.say(r)
+engine.runAndWait()
+
+engine.say("" )
+engine.runAndWait()
+
+r = input('Что мне повторить: ')
+
+engine.say(r)
+engine.runAndWait()
+
+engine.say("" )
+engine.runAndWait()
+
+r = input('Что мне повторить: ')
+
+engine.say(r)
+engine.runAndWait()
+
+engine.say("" )
+engine.runAndWait()
+
+r = input('Что мне повторить: ')
+
+engine.say(r)
+engine.runAndWait()
+
+engine.say("" )
+engine.runAndWait()
+
+r = input('Что мне повторить: ')
+
+engine.say(r)
+engine.runAndWait()
+
+engine.say("" )
+engine.runAndWait()
+
+r = input('Что мне повторить: ')
+
+engine.say(r)
+engine.runAndWait()
+
+engine.say("" )
+engine.runAndWait()
+
+r = input('Что мне повторить: ')
+
+engine.say(r)
+engine.runAndWait()
+
+engine.say("Все мне надоело, это был пока последний цыкл, Перезапускай программу членопопик мелкий" )
+engine.runAndWait()
